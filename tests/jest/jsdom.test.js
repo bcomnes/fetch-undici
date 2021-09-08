0ed0e4ab5cadf93a4e0a6ba4f9d35f54e65e05f9
Bret Comnes <bcomnes@gmail.com>
2021-09-08 12:22:27 -0600

    CI debug

--- tests/jest/jsdom.test.js
+++ tests/jest/jsdom.test.js
@@ -5,6 +5,8 @@
 
 const { fetch, type } = require('fetch-undici')
 
+console.log(require('fetch-undici'))
+
 test('jsdom in mjs', () => {
   expect(type).toBe('node.cjs')
   expect(fetch).toBeDefined()
--- tests/jest/node.test.js
+++ tests/jest/node.test.js
@@ -2,6 +2,8 @@
 
 const { fetch, type } = require('fetch-undici')
 
+console.log(require('fetch-undici'))
+
 test('node in cjs', () => {
   expect(type).toBe('node.cjs')
   expect(fetch).toBeDefined()
